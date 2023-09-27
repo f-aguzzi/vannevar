51b43b24414cf046151d5bf63b8631613fff1971
Federico Aguzzi <federico.aguzzi@icloud.com>
2023-09-27 17:56:55 +0200

    Cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use controller::Controller;
-use vannevar::Journal;
-use view::{link_menu, select_create_journal, start_page};
 
 mod controller;
 mod view;
@@ -11,23 +9,3 @@ fn main() {
 
     c.execute();
 }
-
-/* DISPLAY_JOURNAL
-let pages = vec![String::from("mucca"), String::from("toro"), String::from("bufalo")];
-let j = Journal {
-    date: String::from("2023-04-11"),
-    description: String::from("Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. Questo è un vitellone di prova. "),
-    pages: pages
-};
-view::display_journal(j);
-
-while 1 == 1 {
-
-    }
-*/
-
-/* LINK_MENU
-let list = vec![String::from("cane"), String::from("gatto"), String::from("topo")];
-let selection = link_menu(list);
-println!("{}", selection);
-*/
